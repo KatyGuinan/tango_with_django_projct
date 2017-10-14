2afbecb84fe7dcd783c4871078803a45b25821c3
KatyGuinan <katy.guinan@gmail.com>
2017-10-14 15:47:20 -0400

    3rd commit, added ging

--- tango_with_django_projct/settings.py
+++ tango_with_django_projct/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'tango_with_django_projct','templates')
+STATIC_DIR = os.path.join(BASE_DIR,'tango_with_django_projct','static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'tango_with_django_projct.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_DIRS = [STATIC_DIR,]
 
 WSGI_APPLICATION = 'tango_with_django_projct.wsgi.application'
 
@@ -120,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
